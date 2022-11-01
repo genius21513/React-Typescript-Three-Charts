acc131b7953637e67fa4c56b2df4a2283d86af00
genius21513 <genius21513@gmail.com>
2022-11-01 21:57:06 +0900

    Change component

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import BarCt from './components/BarCt';
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 h-screen">
-      <h3 className='text-center font-bold text-lg pt-12'>Three Charts</h3>
-      <div className='flex mt-12 gap-2 mx-5'>
-        <div className="w-1/5 my-auto">
-          <PieCt />
-        </div>
-        <div className='w-2/5 my-auto'>
-          <BarCt />
-        </div>
-        <div className='w-2/5 my-auto'>
-          <BarCt />
+    <div className="App">
+      <div className='container mx-auto h-screen'>
+        <h3 className='text-center font-bold text-lg pt-12'>Three Charts</h3>
+        <div className='flex mt-12 gap-2 mx-5'>
+          <div className="w-1/5 my-auto">
+            <PieCt />
+          </div>
+          <div className='w-2/5 my-auto'>
+            <BarCt />
+          </div>
+          <div className='w-2/5 my-auto'>
+            <BarCt />
+          </div>
         </div>
       </div>
     </div>
--- src/components/PieCt.tsx
+++ src/components/PieCt.tsx
@@ -1,35 +1,24 @@
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+// import React from 'react';
+import Chart from 'react-apexcharts';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',        
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',        
-      ],
-      borderWidth: 1,
+const data = {
+  options: {
+    chart: {
+      id: 'apexchart-example'
     },
-  ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+  },
+  series: [{
+    name: 'series-1',
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  }]
 };
 
 function PieCt() {
   return (
-    <Pie data={data} />
+    <Chart options={data.options} series={data.series} type="donut" width={500} height={320} />    
   );
 }
 
