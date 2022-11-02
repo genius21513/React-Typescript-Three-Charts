c1149d4c70d7912c6557c0e770520cca79d315a8
genius21513 <genius21513@gmail.com>
2022-11-03 05:00:12 +0900

    Fix misc

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,23 @@ import PieCt from './components/PieCt';
 import HBarCt from './components/HBarCt';
 import HisCt from './components/HisCt';
 
+var dataJson = require('./api/data.json');
+
 Chart.register(...registerables, ChartDataLabels);
 
 function App() {
   return (
     <div className="App min-h-screen overflow-hidden">
       <div className='container mx-auto py-24 px-2'>
-        <h1 className='my-auto text-7xl sm:text-6xl md:text-5xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200'>Three Charts</h1>
+        <div>
+          <h1 className='my-auto text-7xl sm:text-6xl md:text-5xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200'>Three Charts</h1>
+          
+        </div>
         <div className='md:flex flex-column gap-5 items-center mt-12 shadow-md hover:shadow-lg bg-gray-800 rounded-lg py-6'>
           <div className='md:w-2/5 sm:w-full p-2'>
             <HBarCt />
           </div>
-          <div className="md:w-1/5 sm:w-full p-2">
+          <div className="md:w-1/5 w-1/2 p-2 mx-auto">
             <PieCt />
           </div>
           <div className='md:w-2/5 sm:w-full p-2'>
