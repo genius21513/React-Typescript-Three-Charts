c16344f7a584aa0366e399a602bf5c19e9d62364
genius <genius21513@gmail.com>
2022-11-04 03:18:19 +0800

    Fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,33 +11,34 @@ import { ChartDesc, TopDesc } from './components/Desc';
 import { ChartCard } from './components/ChartCard';
 
 
-function App() {
-  React.useEffect(() => {
-    if (typeof registerables === 'undefined') return;
-    Chart.register(...registerables, ChartDataLabels);
-  }, []);
+if (registerables.length > -1 )
+  Chart.register(...registerables, ChartDataLabels);
 
+function App() {
   return (
     <>
-      <div className="App font-mono min-h-screen bg-slate-800 flex">
-        <div className='container mx-auto flex flex-col justify-center'>
-          <TopDesc />
-          <div className='flex flex-col md:flex-row'>
-            <ChartCard className='md:w-1/5'>
-              <PieCt />
-              <ChartDesc desc="Unique Holder Wallets" />
-            </ChartCard>
-            <ChartCard className='md:w-2/5'>
-              <HBarCt />
-              <ChartDesc desc="Unique Holders"/>
-            </ChartCard>          
-            <ChartCard className='md:w-2/5'>
-              <HisCt />            
-              <ChartDesc desc="Holder Distribution"/>
-            </ChartCard>          
+      {        
+        (typeof registerables !== 'undefined') && 
+        <div className="App font-mono min-h-screen bg-slate-800 flex">
+          <div className='container mx-auto flex flex-col justify-center'>
+            <TopDesc />
+            <div className='flex flex-col md:flex-row'>
+              <ChartCard className='md:w-1/5'>
+                <PieCt />
+                <ChartDesc desc="Unique Holder Wallets" />
+              </ChartCard>
+              <ChartCard className='md:w-2/5'>
+                <HBarCt />
+                <ChartDesc desc="Unique Holders"/>
+              </ChartCard>          
+              <ChartCard className='md:w-2/5'>
+                <HisCt />            
+                <ChartDesc desc="Holder Distribution"/>
+              </ChartCard>          
+            </div>
           </div>
         </div>
-      </div>
+      }
     </>
   );
 }
--- src/components/Desc.tsx
+++ src/components/Desc.tsx
@@ -1,7 +1,7 @@
 
 export const TopDesc = () => {
     return (
-        <div className="p-2">
+        <div className="p-4 md:-mt-24">
             <h1 className='text-3xl font-bold text-slate-200 py-2'>@Metakey / Unique MetaKey holders</h1>
             <span className='text-slate-400'>Refered from 
                 <a className='text-blue-700' target="_blank" href='https://dune.com/queries/276792' rel="noreferrer"> https://dune.com/queries/276792</a>
